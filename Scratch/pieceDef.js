7bceb1f9ecbe57ef0cce57bf212f005f385fa048
brendan.gray <brendan.gray@optinum.co.za>
2016-06-09 16:26:35 +0200

    Add piece definition file (not yet in use)

---
+++ Scratch/pieceDef.js
@@ -0,0 +1,15 @@
+
+exports.edge = {
+    nPieces: 12,
+    nOrientations: 2
+};
+
+exports.corner = {
+    nPieces: 8,
+    nOrientations: 3
+};
+
+exports.centre = {
+    nPieces: 6,
+    nOrientations: 1    
+};
