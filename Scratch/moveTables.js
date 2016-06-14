ac9a12424b6b4dda04f48e0a57f8d967f26aeb23
brendan.gray <brendan.gray@optinum.co.za>
2016-06-14 12:14:09 +0200

    New approach to move tables which assumes move tables will always be complete before applying a move (saves time of constantly having to check if a coordinate is already in the table)

--- Scratch/moveTables.js
+++ Scratch/moveTables.js
@@ -1,17 +1,19 @@
 var moves = require("./moves");
 var coords = require("./coords");
 
+var piece = require("./pieces");
+
+
 
 // Initialise MoveTables
 
 var moveTables = [];
 var moveKeys = Object.keys(moves.movesDef); // Note: this is in no particular order
 for (key in  moveKeys) {
-    moveTables[key] = [];
+    // console.log("Intialising Move Table for move", moveKeys[key])
+    moveTables[moveKeys[key]] = [[],[],[],[[]],[]];
 }
 
-
-exports.table = moveTables;
 
 
 var applyMove = function (move, stateCoords) {
@@ -39,7 +41,99 @@ var againState = applyMove("R'", newState);
 console.log('Start 3', startState);
 console.log('  New 3', newState);
 console.log('Again 3', againState);
-
 */
 
+
+var applyMoveCO = function (move, coCoord) {
+    if (!moveTables[move][0][coCoord]) {
+        var coState = coords.invertCoordOri(coCoord, piece.corner);
+        coState = moves.applyMoveCO(move, coState);
+        moveTables[move][0][coCoord] = coords.coordOri(coState, piece.corner);
+    }
+    return moveTables[move][0][coCoord];
+};
+
+
+var applyMoveEO = function (move, eoCoord) {
+    if (!moveTables[move][2][eoCoord]) {
+        var eoState = coords.invertCoordOri(eoCoord, piece.edge);
+        eoState = moves.applyMoveEO(move, eoState);
+        moveTables[move][2][eoCoord] = coords.coordOri(eoState, piece.edge);
+    }
+    return moveTables[move][2][eoCoord];
+};
+
+
+var applyMoveCentre = function (move, cnCoord) {
+
+    if (!moveTables[move][4][cnCoord]) {
+        var cnState = coords.invertCoordPerm(cnCoord, piece.centre);
+        cnState = moves.applyMoveCentr(move, cnState);
+        moveTables[move][4][cnCoord] = coords.coordPerm(cnState, piece.centre);
+    }
+    return moveTables[move][4][cnCoord];
+};
+
+
+var applyMoveCP = function (move, cpCoord) {
+
+    if (!moveTables[move][1][cpCoord]) {
+        var cpState = coords.invertCoordPerm(cpCoord, piece.corner);
+        cpState = moves.applyMoveCP(move, cpState);
+        moveTables[move][1][cpCoord] = coords.coordPerm(cpState, piece.centre);
+    }
+    return moveTables[move][1][cpCoord];
+};
+
+
+var applyMoveEP = function (move, epCoord) {
+    
+    var nextState = [];
+    var found = true;
+    for (var i=0; i < epCoord.length; i++) {
+        if (!moveTables[move][3][i][epCoord[i]]) {
+            found = false;            
+            break;
+        }
+        nextState.push(moveTables[move][3][i][epCoord[i]])
+    }
+    if (found) {
+        return nextState
+    }
+    var epState = coords.invertCoordPermLarge(epCoord, piece.edge);
+    epState = moves.applyMoveEP(move, epState);
+    var ep = coords.coordPermLarge(epState, piece.edge);   
+    
+    for (var i=0; i < epCoord.length; i++) {
+        if (!moveTables[move][3][i]){
+            moveTables[move][3][i] = [];
+        }            
+        moveTables[move][3][i][epCoord[i]] = ep[i];
+    }
+    
+    return ep;
+};
+
+
+
+var applyMoveWithTables = function (move, stateCoords) {
+    
+    var co = applyMoveCO(move, stateCoords[0]);
+    var eo = applyMoveEO(move, stateCoords[2]);
+    
+    var cn = applyMoveCentre(move, stateCoords[4]);
+  
+    var cp = applyMoveCP(move, stateCoords[1]);
+    var ep = applyMoveEP(move, stateCoords[3]);
+        
+    var newStateCoords = [co,cp,eo,ep,cn];
+    
+    return newStateCoords;
+};
+
+
+
+exports.table = moveTables;
+
 exports.applyMove = applyMove;
+exports.applyMoveWithTables = applyMoveWithTables;
---
+++ Scratch/moveTables2.js
@@ -0,0 +1,102 @@
+var moves = require("./moves");
+var coord = require("./coords2");
+
+
+
+var tables = {};
+exports.tables = tables;
+
+
+
+var build333MoveTable = function (move) {
+    
+    var state;
+    tables[move] = {};
+    
+    
+    // CO
+    tables[move][0] = {};
+    for (var hash=0; hash < Math.pow(3,8); hash++) {
+        state = coord.invertHash(hash, 2, 8);
+        tables[move][0][hash] = coord.getHash(moves.applyMoveCO(move, state), 2);        
+    }
+    
+    // EO
+    tables[move][2] = {};
+    for (var hash=0; hash < Math.pow(2, 12); hash++) {
+        state = coord.invertHash(hash, 2, 12);
+        tables[move][2][hash] = coord.getHash(moves.applyMoveEO(move, state), 1);        
+    }
+    
+    // Note: Because CP and EP track positions of groups of four pieces, and 
+    // the positions are independent of the four positions being tracked, 
+    // the same move table can be applied to all groups.  
+    
+    //CP
+    tables[move][1] = {};
+    for (var hash=0; hash < Math.pow(9, 4); hash++) {
+        state = coord.positionsToState(coord.invertHash(hash, 8, 4), [1,2,3,4], 8, 8);
+        // Skip states that are missing a piece.
+        if (state.indexOf(1)<0 || state.indexOf(2)<0 || state.indexOf(3)<0 || state.indexOf(4)<0) {
+            continue;
+        }
+        tables[move][1][hash] = coord.getHash(coord.findPieces(moves.applyMoveCP(move, state), [1,2,3,4], 8), 8);   
+    }
+    
+    //EP
+    tables[move][3] = {};
+    for (var hash=0; hash < Math.pow(13, 4); hash++) {
+        state = coord.positionsToState(coord.invertHash(hash, 12, 4), [1,2,3,4], 12, 12);
+        // Skip states that are missing a piece.
+        if (state.indexOf(1)<0 || state.indexOf(2)<0 || state.indexOf(3)<0 || state.indexOf(4)<0) {
+            continue;
+        }
+        tables[move][3][hash] = coord.getHash(coord.findPieces(moves.applyMoveEP(move, state), [1,2,3,4], 12), 12);  
+    }
+    
+    // Centres
+    tables[move][4] = {};
+    for (var hash=0; hash < Math.pow(7,6); hash++) {
+        state = coord.invertHash(hash, 6, 6);
+        if (state.indexOf(1)<0 || state.indexOf(2)<0 || state.indexOf(3)<0 || 
+            state.indexOf(4)<0 || state.indexOf(5)<0 || state.indexOf(6)<0) {
+            continue;
+        }        
+        tables[move][4][hash] = coord.getHash(moves.applyMoveCentr(move, state), 6);           
+    }
+};
+
+exports.build333MoveTable = build333MoveTable;
+  
+
+
+var moveLookup = function(move, state) {
+    return [
+        tables[move][0][state[0]],
+        tables[move][1][state[1]],
+        tables[move][1][state[2]],
+        tables[move][2][state[3]],
+        tables[move][3][state[4]],
+        tables[move][3][state[5]],
+        tables[move][3][state[6]],
+        tables[move][4][state[7]]
+    ];
+};
+exports.moveLookup = moveLookup;
+
+
+
+// Test cases
+
+/*
+build333MoveTable("R");
+
+var startStateRaw = [[0,0,0,0,0,0,0,0], [1,2,3,4,5,6,7,8], [0,0,0,0,0,0,0,0,0,0,0,0], [1,2,3,4,5,6,7,8,9,10,11,12], [1,2,3,4,5,6]];
+var startState = coord.get333hashes(startStateRaw);
+
+console.log(startState);
+
+console.log(moveLookup("R", startState));
+
+
+/**/
